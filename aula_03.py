1d56bbeb8d6af44a36ef5dba1f1cfa1267ef029a
Celso de Oliveira Silva <devworksacm@gmail.com>
2020-12-21 20:18:38 -0300

    Primeiro commit

---
+++ aula_03.py
@@ -0,0 +1,24 @@
+def soma(a, b):
+    print(f"{a} + {b} é igual a {a + b}.")
+
+def divide(a, b):
+    print(f"{a} dividido por {b} é igual a {a / b}.")
+
+def subtrai(a, b):
+    print(f"{a} - {b} é igual a {a - b}.")
+
+def multiplicacao(a, b):
+    print(f"{a} * {b} = {a * b}")    
+
+def getPDF():
+    print("impressao PDF")
+def getXLS():
+    print("impfessao XLS")
+
+def getImagem():
+    print("funcao que exibe imagem")    
+
+soma(5, 4)
+divide(10, 2)
+subtrai(10, 4)
+multiplicacao(3, 3)
